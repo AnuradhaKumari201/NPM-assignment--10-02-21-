e8afe077f9525ccd515369637ca92c28e9944cfb
AnuradhaKumari201 <anu@codewithanu.com>
2021-03-16 18:46:38 +0530

    Anuradha's assignment

--- src/main/java/ConcatenationOfStrings.java
+++
@@ -1,8 +0,0 @@
-
-    @FunctionalInterface
-    public interface ConcatenationOfStrings {
-       String concat(String str1, String str2, String str3, String str4, String str5, String str6, String str7);
-
-
-    }
-
